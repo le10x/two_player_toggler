91053e8b803d9f4dd7ac197ea203b154397925ee
le10x <leanderdrb@gmail.com>
2026-04-07 20:05:41 -0400

    Enhance PauseLayer with custom button functionality
    Modify the PauseLayer to add a custom button with a click event.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,39 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/PauseLayer.hpp>
+
 using namespace geode::prelude;
 
-$execute {
-    log::info("Hola desde Geode!");
-}
+class $modify(MyPauseLayer, PauseLayer) {
+    bool init() {
+        if (!PauseLayer::init()) return false;
+
+        // Intentamos cargar tu imagen. Si no existe, usamos el botón de Play por defecto.
+        auto buttonSprite = CCSprite::createWithSpriteFrameName("my_button.png");
+        if (!buttonSprite) {
+            buttonSprite = CCSprite::createWithSpriteFrameName("GJ_playBtn_001.png");
+        }
+        
+        auto myButton = CCMenuItemSpriteExtra::create(
+            buttonSprite,
+            this,
+            menu_selector(MyPauseLayer::onMyButtonClick)
+        );
+
+        // Buscamos el menú izquierdo por su ID de Geode
+        if (auto leftMenu = this->getChildByID("left-button-menu")) {
+            myButton->setID("hello-world-button"_spr);
+            leftMenu->addChild(myButton);
+            leftMenu->updateLayout(); 
+        }
+
+        return true;
+    }
+
+    void onMyButtonClick(CCObject* sender) {
+        FLAlertLayer::create(
+            "Prueba",           
+            "Hello World",      
+            "OK"                
+        )->show();
+    }
+};
